9309074392e2304fef88d48920452e6c5a45c0ed
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-02-01 00:15:48 +0530

    comittin update for the server file

---
+++ nodecellar/server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    wine = require('./routes/wines');
+ 
+var app = express();
+ 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  next();
+});
+ 
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app['delete']('/wines/:id', wine.deleteWine);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');
